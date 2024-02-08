54400d3a28cf26ea2a335af8221da53b02dd8803
kusumaarumjati <64127756+kusumaarumjati@users.noreply.github.com>
2024-02-08 15:40:39 +0700

    Add files via upload

---
+++ automation_arum/Test Suites/Test_Fb_DOT.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_Fb_DOT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5313618d-80cf-4fd9-a30b-0442cb89972f</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef162fd0-9dd9-48d1-a89a-c3b4c62a33af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_Fb_DOT/Login_Fb_Positive_case</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>d1d56c59-9358-45b5-af27-a4a5105d4900</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_Fb_DOT/Login_Fb_negative_email</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>e9a4c79a-cc7a-4f8d-837d-55b5d6c77c09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_Fb_DOT/Login_fb_negative_email2</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>f71dbd01-9c7e-4bc1-a1cf-05601d8b6b65</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_Fb_DOT/Login_fb_negative_pass</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
